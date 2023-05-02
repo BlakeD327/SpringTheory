d6d3f7d21e6c1f9526a013121af1dcf936e4d29c
Bryan253 <bryan530@uw.edu>
2023-05-02 14:35:44 -0700

    Organized the code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,31 +5,33 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
+    // Const
+    public const int MAX_HP = 100;
+    
+    // Player related stats
     public float speed = 10f;
-    public bool grounded = true;
+    public int inventory = 0;
+    public int currentHealth = MAX_HP;
+    public float jumpPower = 10f;
+
+    // Internal use of components
     private Rigidbody2D rb2d;
-    public float jumpPower;
     private BoxCollider2D boxCollider2D;
-    public int maxHealth = 100;
-    public int currentHealth;
     public HealthBar healthBar;
     public GameObject levelCompleteUI;
+    [SerializeField] private LayerMask platformsLayerMask;
 
-
-    public static int inventory = 0;
-
+    // Projectile info
     public GameObject Orb;
     public float projectileSpeed = 10f;
 
-    [SerializeField] private LayerMask platformsLayerMask;
 
     // Start is called before the first frame update
     void Start()
     {        
         rb2d = rb2d = GetComponent<Rigidbody2D> ();
         boxCollider2D = GetComponent<BoxCollider2D>();
-        currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
+        healthBar.SetMaxHealth(MAX_HP);
     }
 
     // Update is called once per frame
@@ -37,57 +39,59 @@ public class Player : MonoBehaviour
     {
         Movement();
 
+        // If left click is pressed
+        if(Input.GetMouseButtonDown(0))
+            Shoot();
+
         //if for testing healthbar slider set to 'k' key
-        if (Input.GetKeyDown(KeyCode.K))
+        if(Input.GetKeyDown(KeyCode.K))
         {
             TakeDamage(5);
-        }
-        
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Get the mouse position in world space
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            // Calculate the direction vector between the player and the mouse position
-            Vector2 direction = mousePos - transform.position;
-
-            // Normalize the direction vector to have a magnitude of 1
-            direction.Normalize();
-
-            // Calculate the angle in degrees between the direction vector and the x-axis
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-            // Get the bounds of the player's box collider
-            Bounds bounds = GetComponent<BoxCollider2D>().bounds;
-
-            // Calculate the spawn point of the projectile based on the direction vector
-            Vector3 spawnPos = bounds.center + new Vector3(direction.x, direction.y, 0f) * (bounds.extents.x + Orb.GetComponent<BoxCollider2D>().bounds.extents.x);
-
-            // Create a new projectile object at the spawn point
-            GameObject projectile = Instantiate(Orb, spawnPos, Quaternion.identity);
-
-            // Set the velocity of the projectile to be in the direction of the mouse click
-            projectile.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
-
-            // Rotate the projectile to face the direction of the mouse click
-            projectile.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
+        }   
     }
 
-    private void Movement()
+    void Movement()
     {
         var x = Input.GetAxis("Horizontal");
         var v2 = Vector2.zero;
         v2.x = x * speed;
 
         if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
-        {
             v2.y = jumpPower;
-        }
         else
             v2.y = rb2d.velocity.y;
 
         rb2d.velocity = v2; 
+    }
+
+    void Shoot()
+    {
+        // Get the mouse position in world space
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Calculate the direction vector between the player and the mouse position
+        Vector2 direction = mousePos - transform.position;
+
+        // Normalize the direction vector to have a magnitude of 1
+        direction.Normalize();
+
+        // Calculate the angle in degrees between the direction vector and the x-axis
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // Get the bounds of the player's box collider
+        Bounds bounds = GetComponent<Collider2D>().bounds;
+
+        // Calculate the spawn point of the projectile based on the direction vector
+        Vector3 spawnPos = bounds.center + new Vector3(direction.x, direction.y, 0f) * (bounds.extents.x + Orb.GetComponent<Collider2D>().bounds.extents.x);
+
+        // Create a new projectile object at the spawn point
+        GameObject projectile = Instantiate(Orb, spawnPos, Quaternion.identity);
+
+        // Set the velocity of the projectile to be in the direction of the mouse click
+        projectile.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
+
+        // Rotate the projectile to face the direction of the mouse click
+        projectile.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
     void TakeDamage(int damage) 
@@ -96,7 +100,7 @@ public class Player : MonoBehaviour
         healthBar.SetHealth(currentHealth);
     }
 
-    private bool IsGrounded()
+    bool IsGrounded()
     {
         RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, 
             boxCollider2D.bounds.size, 0f, Vector2.down, 0.1f, platformsLayerMask);
