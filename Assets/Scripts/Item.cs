ba65a6f4c82025f2570423518fa25d8acdd5d062
Bryan253 <bryan530@uw.edu>
2023-05-07 17:04:18 -0700

    Implements inventory

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,6 +7,9 @@ public class Item : ScriptableObject
 {
     public new string name;
     public int weight;
+    public float bounciness;
     public Sprite img;
     public Color color;
+    public int maxBounce;
+    public int ID;
 }
